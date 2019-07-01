23a4a6d2f01290528a0272afafbca1185aa33f96
chizh273 <chizh273@gmail.com>
2019-07-01 20:29:14 +0300

    add rotating

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -12,20 +12,30 @@ export const generateCanvas = (width, height) => {
 
 export const generateFigure = (points, canvasWidth, canvasHeight) => {
   const size = Math.min(canvasWidth, canvasHeight)
+  const step = size / points / 2
+  const rangedArray = range(1, points + 1);
   const offset = {
-    x: canvasWidth / 3,
+    x: canvasWidth / 2,
     y: canvasHeight / 2,
   }
 
-  return range(points + 1, 1, -1).map(i => {
-    return applyOffset(offset, {
-      x: size / i,
-      y: 0,
-    })
-  })
+  const negativePart = rangedArray.map(i =>
+    applyOffset(offset, { x: step * i * -1, y: 0 })
+  )
+
+  const positivePart = rangedArray.map(i =>
+    applyOffset(offset, { x: step * i, y: 0 })
+  )
+
+  return [...negativePart, ...positivePart]
 }
 
 export const applyOffset = (offset, point) => ({
   x: offset.x + point.x,
   y: offset.y + point.y,
+})
+
+export const applyRotate = (angle, point) => ({
+  x: point.x * Math.cos(angle) - point.y * Math.sin(angle),
+  y: point.x * Math.sin(angle) + point.y * Math.cos(angle),
 })
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,14 @@
-import { generateCanvas, generateFigure } from './common'
+import { applyRotate, generateCanvas, generateFigure } from './common'
 
 const WIDTH = window.innerWidth
 const HEIGHT = window.innerHeight
-const MAX_POINTS_COUNT = 100
+const MAX_POINTS_COUNT = 50
 // const MIN_POINTS_COUNT = 20
+
+export const DEG_180 = 180
+export const RADIAN_IN_ONE_DEG = Math.PI / DEG_180
+
+let angle = 0
 
 const canvas = generateCanvas(WIDTH, HEIGHT)
 document.body.appendChild(canvas)
@@ -11,14 +16,26 @@ document.body.appendChild(canvas)
 const ctx = canvas.getContext('2d')
 const points = generateFigure(MAX_POINTS_COUNT, WIDTH, HEIGHT)
 
-ctx.strokeStyle = 'red'
-ctx.strokeWidth = 10
-ctx.moveTo(points[0].x, points[0].y)
+function render() {
+  ctx.strokeStyle = 'red'
+  ctx.fillStyle = 'red'
+  ctx.strokeWidth = 10
+  ctx.moveTo(points[0].x, points[0].y)
 
-points.map(point => {
-  ctx.lineTo(point.x, point.y)
-  ctx.arc(point.x, point.y, 10, 0, 360)
-})
+  points.map((point, i) => {
+    points[i] = applyRotate(angle, point)
 
-ctx.stroke()
-ctx.closePath()
+    ctx.lineTo(point.x, point.y)
+    ctx.arc(point.x, point.y, 2, 0, 360)
+  })
+
+  ctx.stroke()
+  ctx.fill()
+  ctx.closePath()
+
+  angle += RADIAN_IN_ONE_DEG
+
+  requestAnimationFrame(render)
+}
+
+render()
